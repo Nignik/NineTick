5663e4b044ab438668ad0ad12c5447a027c72965
Nignik <maskymilian.olbrys@gmail.com>
2025-07-21 16:33:20 +0200

    Better centered board

--- scripts/Board.cs
+++ scripts/Board.cs
@@ -3,11 +3,12 @@ using System;
 using Godot.Collections;
 using System.Linq.Expressions;
 
-public partial class Board : Node
+public partial class Board : Node2D
 {
     [Export] public PackedScene TileScene { get; set; }
 
-    private const int _width = 9, _height = 9;
+    private const int _gap = 3;
+    private const int _size = 9;
     private Tile[,] _tiles;
 
     private PlayerColor[,] _tileColors;
@@ -15,24 +16,34 @@ public partial class Board : Node
 
     public override void _Ready()
     {
-        _tiles = new Tile[_height, _width];
-
-        for (int r = 0; r < _height; r++)
+        // Initialize the tiles
+        _tiles = new Tile[_size, _size];
+        for (int r = 0; r < _size; r++)
         {
-            for (int c = 0; c < _width; c++)
+            for (int c = 0; c < _size; c++)
             {
                 _tiles[r, c] = TileScene.Instantiate<Tile>();
                 float offset = _tiles[r, c].GetSizeInPixels();
-                _tiles[r, c].Position = new Vector2(c * offset, r * offset);
+                _tiles[r, c].Position = new Vector2(
+                    c * offset + (c/3) * _gap,
+                    r * offset + (r/3) * _gap
+                );
                 _tiles[r, c].Initialize(r, c);
 
                 AddChild(_tiles[r, c]);
             }
         }
 
-        _tileColors = new PlayerColor[_height, _width];
-        for (int r = 0; r < _height; r++)
-            for (int c = 0; c < _width; c++)
+        // Position the board
+        Vector2 screenSize = GetViewport().GetVisibleRect().Size;
+        Tile last = _tiles[_size - 1, _size - 1];
+        float pixelSize = last.Position.X * Scale.X + last.GetSizeInPixels();
+        Position = (screenSize - new Vector2(pixelSize, pixelSize)) * 0.5f;
+
+        // Color the tiles
+        _tileColors = new PlayerColor[_size, _size];
+        for (int r = 0; r < _size; r++)
+            for (int c = 0; c < _size; c++)
                 _tileColors[r, c] = PlayerColor.None;
 
 
